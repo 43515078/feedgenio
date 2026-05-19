e2da1e65e2515de90a4840192e60ae563ab9bc34
43515078 <52327797+43515078@users.noreply.github.com>
2026-05-19 10:34:24 -0500

    Add Ingredient type and defaultIngredients array

---
+++ lib/ingredients.ts
@@ -0,0 +1,156 @@
+export type Ingredient = {
+  id: string;
+  name: string;
+  price: number;
+  min: number;
+  max: number;
+  nutrients: {
+    energy: number;
+    protein: number;
+    lysine: number;
+    methionine: number;
+    metCys: number;
+    calcium: number;
+    availablePhosphorus: number;
+    sodium: number;
+  };
+};
+
+export const defaultIngredients: Ingredient[] = [
+  {
+    id: "maiz",
+    name: "Maíz",
+    price: 1.32,
+    min: 40,
+    max: 70,
+    nutrients: {
+      energy: 3350,
+      protein: 7.7,
+      lysine: 0.24,
+      methionine: 0.17,
+      metCys: 0.35,
+      calcium: 0.03,
+      availablePhosphorus: 0.08,
+      sodium: 0.02
+    }
+  },
+  {
+    id: "soya",
+    name: "Torta de soya 46%",
+    price: 1.7,
+    min: 5,
+    max: 35,
+    nutrients: {
+      energy: 2450,
+      protein: 46,
+      lysine: 2.85,
+      methionine: 0.62,
+      metCys: 1.35,
+      calcium: 0.3,
+      availablePhosphorus: 0.29,
+      sodium: 0.02
+    }
+  },
+  {
+    id: "aceite",
+    name: "Aceite",
+    price: 3.5,
+    min: 0,
+    max: 5,
+    nutrients: {
+      energy: 8800,
+      protein: 0,
+      lysine: 0,
+      methionine: 0,
+      metCys: 0,
+      calcium: 0,
+      availablePhosphorus: 0,
+      sodium: 0
+    }
+  },
+  {
+    id: "carbonato",
+    name: "Carbonato de calcio",
+    price: 0.25,
+    min: 6,
+    max: 11,
+    nutrients: {
+      energy: 0,
+      protein: 0,
+      lysine: 0,
+      methionine: 0,
+      metCys: 0,
+      calcium: 38,
+      availablePhosphorus: 0,
+      sodium: 0
+    }
+  },
+  {
+    id: "dcp",
+    name: "Fosfato dicálcico",
+    price: 2.8,
+    min: 0.5,
+    max: 2.5,
+    nutrients: {
+      energy: 0,
+      protein: 0,
+      lysine: 0,
+      methionine: 0,
+      metCys: 0,
+      calcium: 23,
+      availablePhosphorus: 18,
+      sodium: 0
+    }
+  },
+  {
+    id: "sal",
+    name: "Sal común",
+    price: 0.6,
+    min: 0.2,
+    max: 0.4,
+    nutrients: {
+      energy: 0,
+      protein: 0,
+      lysine: 0,
+      methionine: 0,
+      metCys: 0,
+      calcium: 0,
+      availablePhosphorus: 0,
+      sodium: 39
+    }
+  },
+  {
+    id: "metionina",
+    name: "DL-Metionina",
+    price: 18,
+    min: 0,
+    max: 0.35,
+    nutrients: {
+      energy: 0,
+      protein: 0,
+      lysine: 0,
+      methionine: 99,
+      metCys: 99,
+      calcium: 0,
+      availablePhosphorus: 0,
+      sodium: 0
+    }
+  },
+  {
+    id: "lisina",
+    name: "Lisina HCL",
+    price: 9,
+    min: 0,
+    max: 0.35,
+    nutrients: {
+      energy: 0,
+      protein: 0,
+      lysine: 78,
+      methionine: 0,
+      metCys: 0,
+      calcium: 0,
+      availablePhosphorus: 0,
+      sodium: 0
+    }
+  }
+];
